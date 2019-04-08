600f99f07c6a7761e92893f9c9e1bc23c7e83db4
Serhii Pavlovskyi <spavlovskyi@lohika.com>
2019-04-08 20:41:27 +0300

    add artifactory remote

--- prj1/global_setup.bat
+++ prj1/global_setup.bat
@@ -1,3 +1,5 @@
 pip3 install --upgrade conan meson ninja conan
 if %errorlevel% neq 0 exit /b %errorlevel%
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add -f -i 0 presentation_artifactory http://localhost:8082/artifactory/api/conan/conan-local
+if %errorlevel% neq 0 exit /b %errorlevel%
--- prj1/global_setup.sh
+++ prj1/global_setup.sh
@@ -7,4 +7,5 @@ echo $PATH | grep -q $(python3 -m site --user-base)/bin || {
 }
 pip3 install --user --upgrade meson ninja conan
 conan remote add -f bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add -f -i 0 presentation_artifactory http://localhost:8082/artifactory/api/conan/conan-local
 rm -rf out
